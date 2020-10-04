6db388c58b535d22c209f14da5c77501d601443d
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-04 23:09:27 +0300

    cleanup

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,8 +1,6 @@
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
 
-console.debug('Put the background scripts here.');
-
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
   //Send message to content Script -> Page was changed
   //or execute parser from here 
