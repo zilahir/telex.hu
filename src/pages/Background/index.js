07865fdfb5c283228c19d8306cc14b6022c02208
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-05 01:45:14 +0300

    cleanup

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -3,10 +3,7 @@ import '../../assets/img/icon-128.png';
 import { printLine } from '../Content/modules/print';
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  printLine(details)
   if (details.url !== 'https://www.telex.hu/' && details.url.indexOf('https://www.telex.hu/' !== - 1)) {
-    // chrome.tabs.executeScript(details.tabId, {file: "contentScriptSubPage.bundle.js"});
-    printLine(details.tabId)
     chrome.tabs.sendMessage(details.tabId, {
       isArticle: true,
     })
