9a914ad467b6ab7e946c8c20da83a57f920369da
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-08 01:42:22 +0300

    added redux-persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,18 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import createChromeStorage from 'redux-persist-chrome-storage'
 
 import article from './reducers/article'
 import revisions from './reducers/reviews'
 import user from './reducers/user'
+
+const storage = createChromeStorage(window.chrome, 'sync')
+
+const persistConfig = {
+	key: 'root',
+	storage,
+}
 
 const rootReducer = combineReducers({
 	article,
@@ -11,11 +20,12 @@ const rootReducer = combineReducers({
 	user,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const configureStore = () => createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const configureStore = () => createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export const store = configureStore()
-
-export default configureStore
+export const persistor = persistStore(store)
