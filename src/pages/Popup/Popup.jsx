b3fd3d28b5a7e7501cf179f44b32c7364b9f2bb5
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-08 01:58:08 +0300

    fixing linter errors

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useStore } from 'react-redux';
+import React, { useEffect, useState } from 'react'
+import { useSelector, useStore } from 'react-redux'
 
-import Header from '../../components/Header/index';
-import Login from '../../components/Login';
+import Header from '../../components/Header/index'
+import Login from '../../components/Login'
 
-import './index.css';
-import styles from './Popup.module.scss';
+import './index.css'
+import styles from './Popup.module.scss'
 
 const Popup = () => {
-  const [isLoggedIn, toggleLoggedIn] = useState(
-    useSelector(state => state.user.user)
-  );
-  const reduxStore = useStore();
-  console.debug('store', reduxStore.getState());
-  console.debug('isLoggedIn', isLoggedIn);
-  return (
-    <div className={styles.rootContainer}>
-      <Header isLoggedIn={isLoggedIn} />
-      {!isLoggedIn && <Login />}
-    </div>
-  );
-};
+	const [isLoggedIn, toggleLoggedIn] = useState(
+		useSelector(state => state.user.user),
+	)
+	const reduxStore = useStore()
+	console.debug('store', reduxStore.getState())
+	console.debug('isLoggedIn', isLoggedIn)
+	return (
+		<div className={styles.rootContainer}>
+			<Header isLoggedIn={isLoggedIn} />
+			{!isLoggedIn && <Login />}
+		</div>
+	)
+}
 
-export default Popup;
+export default Popup
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 
 import { store } from '../../store/configureStore'
 
-import Popup from './Popup'
+import Popup from './popup'
 
 render(
 	<Provider store={store}>
