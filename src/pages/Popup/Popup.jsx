21b3af4071e88c71a63315ef13625b7a9bcd3c7a
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-08 02:12:07 +0300

    adjustments

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useStore } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 import Header from '../../components/Header/index'
 import Login from '../../components/Login'
@@ -8,16 +8,11 @@ import './index.css'
 import styles from './Popup.module.scss'
 
 const Popup = () => {
-	const [isLoggedIn, toggleLoggedIn] = useState(
-		useSelector(state => state.user.user),
-	)
-	const reduxStore = useStore()
-	console.debug('store', reduxStore.getState())
-	console.debug('isLoggedIn', isLoggedIn)
+	const user = useSelector(state => state.user.user)
 	return (
 		<div className={styles.rootContainer}>
-			<Header isLoggedIn={isLoggedIn} />
-			{!isLoggedIn && <Login />}
+			<Header isLoggedIn={user} />
+			{!user && <Login isLoggedIn={user} />}
 		</div>
 	)
 }
