6805d7bbd225fea4c8e337b033ab66aed25eb902
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-10 02:39:12 +0300

    implementing dark mode

--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import DarkModeToggle from 'react-dark-mode-toggle'
 
 import styles from './Settings.module.scss'
+import { toggleDarkMode } from '../../store/actions/misc'
 
 /**
  * @author zilahir
@@ -9,14 +11,22 @@ import styles from './Settings.module.scss'
  * */
 
 const Settings = () => {
-	const [isDarkMode, setIsDarkMode] = useState(() => false)
+	const darkMode = useSelector(state => state.misc.darkmode)
+	const [isDarkMode, setIsDarkMode] = useState(darkMode)
+	const dispatch = useDispatch()
+
+	function handleDarkMode() {
+		const newValue = !isDarkMode
+		setIsDarkMode(newValue)
+		dispatch(toggleDarkMode)
+	}
 	return (
 		<div className={styles.settingsRootContainer}>
 			<div className={styles.oneItem}>
 				<p>Dark mód</p>
 				<DarkModeToggle
 					size={50}
-					onChange={setIsDarkMode}
+					onChange={() => handleDarkMode()}
 					checked={isDarkMode}
 				/>
 			</div>
