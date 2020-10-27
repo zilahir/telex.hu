23c5b45e4fa7c1e159e9ec3c3eb7c5e834d94ca5
Richard Zilahi <richard.zilahi@salesframe.com>
2020-10-27 02:15:44 +0200

    fixed darkmode

--- src/pages/Content/modules/darkmode.js
+++ src/pages/Content/modules/darkmode.js
@@ -11,7 +11,7 @@ export function createDarkMode() {
 		const darkModeLiElement = document.createElement('li')
 		const darkModeSpanElement = document.createElement('span')
 		chrome.storage.local.get('darkmode', value => {
-			store.dispatch(toggleDarkMode(value.darkmode))
+			store.dispatch(toggleDarkMode(value.darkmode || false))
 		})
 		darkModeLiElement.setAttribute('id', 'toggle-darkmode')
 		darkModeSpanElement.innerHTML = moonIcon
@@ -22,12 +22,14 @@ export function createDarkMode() {
 				darkModeLiElement.setAttribute('class', 'darkmode')
 				darkModeSpanElement.innerHTML = sunIcon
 				store.dispatch(toggleDarkMode(true))
+				chrome.storage.local.set({ darkmode: true })
 			} else if (document.body.classList.contains('darkmode')) {
 				document.body.classList.remove('darkmode')
 				document.body.classList.add('lightmode')
 				darkModeLiElement.classList.remove('darkmode')
 				darkModeSpanElement.innerHTML = moonIcon
 				store.dispatch(toggleDarkMode(false))
+				chrome.storage.local.set({ darkmode: false })
 			}
 		})
 		darkModeLiElement.append(darkModeSpanElement)
